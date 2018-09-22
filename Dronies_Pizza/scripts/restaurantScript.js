15295547539b91d794a5566d4841498671f85745
Nathan Hyatt <nhyatt0313@gmail.com>
2018-09-21 19:40:59 -0600

    added function to restaurantScript.js to check validity of form. added noscript functionality

---
+++ Dronies_Pizza/scripts/restaurantScript.js
@@ -0,0 +1,75 @@
+
+/*
+	Name: Nathan Hyatt
+	Date Created: 09/21/2018
+	Most Recent Revision: 09/21/2018
+*/
+
+function formValidation() {
+	// initialize boolean values
+	var validForm = false;
+	var validName = false;
+	var validEmail = false;
+	var validPhone = false;
+	// test the forms required fields
+	// these forms are already 'required' so we don't need to test for empty inputs
+	if (!validName) {
+		var name = document.forms["contact-us"]["name"].value;
+		var namePattern = /[^a-z ]/i; // Regular expression for /[not(^) a letter(a-z) or space ( )]/case-insensitive(i)
+		var match = name.match(namePattern); // returns null if there are no characters matching namePattern
+		
+		if (match != null) {
+			// contains a character that is not a letter
+			alert("invalid character in name field");
+		}
+		else {
+			// name field is valid
+			validName = true;
+		}
+	}
+	if (!validEmail) {
+		var email = document.forms["contact-us"]["email"].value;
+		// email type already handles the '@' symbol
+		// need a pattern that has at least 1 '.'
+		var ep = /[.]+/; // contains at least one '.'
+		var epMatch = email.match(ep);
+		if (epMatch != null) {
+			validEmail = true;
+		}
+		else {
+			alert("invalid email address");
+		}
+	}
+	if (!validPhone) {
+		var phone = document.forms["contact-us"]["phone"].value;
+		// we need an input that is between 10 and 11 characters, excluding -'s
+		// remove the -'s, spaces and ()'s from the number
+		var phoneArray = phone.split('');
+		for (var i=0; i<phoneArray.length; i++) {
+			if ( isNaN(phoneArray[i]) ) {
+				phoneArray.splice(i,1);
+			}
+		}
+		// check length of phone number
+		if (phoneArray.length < 10 || phoneArray.length > 11) {
+			alert("invalid phone number: please use 10 or 11 digit phone number");
+		}
+		else {
+			validPhone = true;
+		}
+	}
+	validForm = validName && validEmail && validPhone;
+	if (validForm) {
+		alert("The provided information is valid. Thank you for your submission!");
+	} 
+	// don't need an else statement here because all invalid entries were already considered
+	// save the other values in variables as though the form were to be submitted
+	// reason for inquiry
+	var reason = document.forms["contact-us"]["reason"].value;
+	// additional information
+	var info = document.forms["contact-us"]["info"].value;
+	// have they visited the restaurant?
+	var visited = document.forms["contact-us"]["radio-input"].value;
+	// best days to contact
+	var best = document.forms["contact-us"]["checkbox-selection"].value;
+}
